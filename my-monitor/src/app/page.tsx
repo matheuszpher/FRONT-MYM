97bc3df73d1784264c409be10b50148e36c21141
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-07 15:01:39 -0300

    :tada: init: initial project

---
+++ my-monitor/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Dashboard } from '@/templates'
+
+export default function Home() {
+  return (
+    <main className="items-center justify-center flex">
+      <Dashboard />
+    </main>
+  )
+}
---
+++ my-monitor/src/templates/dashboard/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div>Hello, You are logged in</div>
+}
+
+export default Dashboard
