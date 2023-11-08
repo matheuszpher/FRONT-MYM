e9af18286e2f11cba56e98413e73ad26b5917373
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-08 16:06:18 -0300

    :sparkles: feat: add register validation with Zod and React Hook Form

--- my-monitor/src/templates/cadastro/Cadastro.tsx
+++ my-monitor/src/templates/cadastro/Cadastro.tsx
@@ -1,5 +1,14 @@
+'use client'
+
+import { FormCadastro } from '@/templates/cadastro/components'
+
 const Cadastro = () => {
-  return <div>Cadastro</div>
+  return (
+    <div>
+      <h1>Cadastro</h1>
+      <FormCadastro />
+    </div>
+  )
 }
 
 export default Cadastro
