1b933a4c200ceadd3e3d64e5083c602bf2e9041b
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-10 10:44:03 -0300

    :sparkles: style: Coolvetica font and update Tailwind config.

--- my-monitor/tailwind.config.js
+++ my-monitor/tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        coolvetica: ['coolvetica'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
