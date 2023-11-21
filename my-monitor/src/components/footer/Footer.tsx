c4f3c0c6928ca4d165ec0fb0ead6704d5e9a3442
Milena Chaves <milenchaves.study@gmail.com>
2023-11-21 14:02:03 -0300

    fix: minor errors

--- my-monitor/src/components/footer/Footer.tsx
+++ my-monitor/src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <footer className="flex justify-center items-center text-white bg-[#031229] mt-24 h-14 m-h-16 gap-12 w-full">
+        <footer className="flex justify-center items-center text-xs text-white h-[36px] w-[1440px]-full bg-slate-900 gap-10">
             <button>Contrato do Usuário</button>
             <button>Política de Privacidade</button>
             <button>Política de Cookies</button>
--- my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
+++ my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
@@ -1,16 +1,15 @@
 import { Cadastro } from '@/templates'
-import { AsideHomePageCadastro, Footer, HeaderHomepage } from '@/components/homepage'
+import { AsideHomePageCadastro, HeaderHomepage } from '@/components/homepage'
+import { Footer } from '@/components/footer'
 
 const HomepageCadastro = () => {
   return (
-    <div className='flex flex-col w-full'>
-      <div className="flex min-h-screen min-w-full flex-col gap-2 ">
+      <div className="flex min-h-screen min-w-full flex-col gap-2 h-screen overflow-hidden">
         <HeaderHomepage logoSrc="/images/LogoBranca.png" textColor="text-slate-100" />
         <div className="flex flex-1 flex-row items-center justify-between overflow-hidden  px-[73px]">
           <AsideHomePageCadastro />
           <Cadastro />
         </div>
-      </div>
       <Footer/>
     </div>
   )
