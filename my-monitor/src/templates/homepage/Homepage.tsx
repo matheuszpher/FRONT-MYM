ad299a486f846c1ae1e2a478054a5312dd162d1b
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-22 09:35:30 -0300

    :recycle: refactor: footer and pages

--- my-monitor/src/templates/homepage/Homepage.tsx
+++ my-monitor/src/templates/homepage/Homepage.tsx
@@ -4,7 +4,7 @@ import { Footer } from '@/components/footer'
 
 const Homepage = () => {
   return (
-    <div className="flex min-h-screen min-w-full flex-col gap-2 h-screen overflow-hidden">
+    <div className="flex h-screen min-h-screen min-w-full flex-col gap-2 overflow-hidden">
       <HeaderHomepage />
       <div className="flex flex-1 flex-col">
         <div className="flex flex-1 flex-row items-center justify-between overflow-hidden px-[73px]">
@@ -13,7 +13,7 @@ const Homepage = () => {
           <Login />
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
+++ my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
@@ -4,13 +4,16 @@ import { Footer } from '@/components/footer'
 
 const HomepageCadastro = () => {
   return (
-      <div className="flex min-h-screen min-w-full flex-col gap-2 h-screen overflow-hidden">
-        <HeaderHomepage logoSrc="/images/LogoBranca.png" textColor="text-slate-100" />
-        <div className="flex flex-1 flex-row items-center justify-between overflow-hidden  px-[73px]">
-          <AsideHomePageCadastro />
-          <Cadastro />
-        </div>
-      <Footer/>
+    <div className="flex h-screen w-full flex-col gap-2 overflow-hidden">
+      <HeaderHomepage
+        logoSrc="/images/LogoBranca.png"
+        textColor="text-slate-100"
+      />
+      <div className="flex flex-1 flex-row items-center justify-between overflow-hidden  px-[73px]">
+        <AsideHomePageCadastro />
+        <Cadastro />
+      </div>
+      <Footer />
     </div>
   )
 }
