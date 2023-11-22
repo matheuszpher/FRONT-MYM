fbb236905ecc3a5ba090a2bc8bd54db7588cd7b8
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-22 10:30:54 -0300

    :recycle: refactor styles in Homepage and Cadastro components

--- my-monitor/src/templates/homepage/Homepage.tsx
+++ my-monitor/src/templates/homepage/Homepage.tsx
@@ -4,7 +4,7 @@ import { Footer } from '@/components/footer'
 
 const Homepage = () => {
   return (
-    <div className="flex h-screen min-h-screen min-w-full flex-col gap-2 overflow-hidden">
+    <div className="flex h-screen min-h-screen min-w-full flex-col overflow-hidden">
       <HeaderHomepage />
       <div className="flex flex-1 flex-col">
         <div className="flex flex-1 flex-row items-center justify-between overflow-hidden px-[73px]">
--- my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
+++ my-monitor/src/templates/homepagecadastro/HomepageCadastro.tsx
@@ -4,14 +4,16 @@ import { Footer } from '@/components/footer'
 
 const HomepageCadastro = () => {
   return (
-    <div className="flex h-screen w-full flex-col gap-2 overflow-hidden">
+    <div className="flex h-screen w-full flex-col overflow-hidden">
       <HeaderHomepage
         logoSrc="/images/LogoBranca.png"
         textColor="text-slate-100"
       />
-      <div className="flex flex-1 flex-row items-center justify-between overflow-hidden  px-[73px]">
-        <AsideHomePageCadastro />
-        <Cadastro />
+      <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-row items-center justify-between overflow-hidden  px-[73px]">
+          <AsideHomePageCadastro />
+          <Cadastro />
+        </div>
       </div>
       <Footer />
     </div>
